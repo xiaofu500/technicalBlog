998b98c60b4fdda141cfdbc8364e75e610b4fef0
小福 <995333824@qq.com>
2025-12-12 08:57:16 +0800

    Updates

---
+++ assets/doc_MyselfInfo_plan_index.md.BLzbGl9k.js
@@ -0,0 +1,8 @@
+import{ax as n,c as s,o as e,b0 as p}from"./chunks/framework.CFJOrnnk.js";const h=JSON.parse('{"title":"25年","description":"","frontmatter":{},"headers":[],"relativePath":"doc/MyselfInfo/plan/index.md","filePath":"doc/MyselfInfo/plan/index.md"}'),t={name:"doc/MyselfInfo/plan/index.md"};function l(i,a,o,c,d,r){return e(),s("div",null,[...a[0]||(a[0]=[p(`<h1 id="_25年" tabindex="-1">25年 <a class="header-anchor" href="#_25年" aria-label="Permalink to &quot;25年&quot;">​</a></h1><h2 id="第50周" tabindex="-1">第50周 <a class="header-anchor" href="#第50周" aria-label="Permalink to &quot;第50周&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>周三</span></span>
+<span class="line"><span>自学任务：</span></span>
+<span class="line"><span>1.自动化部署（已完成个人技术博客自动化部署）</span></span>
+<span class="line"><span>2.面试总结</span></span>
+<span class="line"><span>3.数据库自动化备份</span></span>
+<span class="line"><span>周四：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>周五：</span></span></code></pre></div>`,3)])])}const f=n(t,[["render",l]]);export{h as __pageData,f as default};
