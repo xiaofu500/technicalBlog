7bef606e2597446e2763bb9b549cd48be5104d0d
小福 <995333824@qq.com>
2025-12-18 12:48:00 +0800

    Updates

---
+++ assets/doc_MyselfInfo_plan_index.md.B9XPaDto.js
@@ -0,0 +1,26 @@
+import{Q as e,X as t,_t as n,s as r}from"./chunks/framework.CEEeIAwU.js";const i=JSON.parse(`{"title":"25 年","description":"","frontmatter":{},"headers":[],"relativePath":"doc/MyselfInfo/plan/index.md","filePath":"doc/MyselfInfo/plan/index.md"}`);var a={name:`doc/MyselfInfo/plan/index.md`};function o(r,i,a,o,s,c){return n(),t(`div`,null,[...i[0]||=[e(`<h1 id="_25-年" tabindex="-1">25 年 <a class="header-anchor" href="#_25-年" aria-label="Permalink to &quot;25 年&quot;">​</a></h1><h2 id="第-50-周" tabindex="-1">第 50 周 <a class="header-anchor" href="#第-50-周" aria-label="Permalink to &quot;第 50 周&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>周三</span></span>
+<span class="line"><span>自学任务：</span></span>
+<span class="line"><span>1.自动化部署（已完成个人技术博客自动化部署）</span></span>
+<span class="line"><span>2.面试总结</span></span>
+<span class="line"><span>3.数据库自动化备份</span></span>
+<span class="line"><span>周四：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>周五：</span></span></code></pre></div><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>最近一段时间的学习：</span></span>
+<span class="line"><span> 依赖AI过于严重，没有自己思考的过程，知识不过大脑，导致学完记不住。</span></span>
+<span class="line"><span> 需要改变学习策略：</span></span>
+<span class="line"><span>1.之前已经看过一遍视频教程，2刷放2倍速，在过程中手写记录笔记</span></span>
+<span class="line"><span>2.根据学习视频及笔记 完全独立实现，不借助AI 实现项目</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>工作效率：</span></span>
+<span class="line"><span>1.恺德工作中</span></span>
+<span class="line"><span>当前是在不断试错，效率不高。</span></span>
+<span class="line"><span>正常开发思维：</span></span>
+<span class="line"><span>1. 先思考，再实现</span></span>
+<span class="line"><span>2. 先写注释，再写代码</span></span>
+<span class="line"><span>3. 先写测试用例，再写代码</span></span>
+<span class="line"><span>针对技术难题：</span></span>
+<span class="line"><span>1. 先分析问题，再思考解决方案</span></span>
+<span class="line"><span>2. 先写伪代码，再实现</span></span>
+<span class="line"><span>针对工作时间及工作量预估</span></span>
+<span class="line"><span>1. 预估工作时间：根据项目需求、项目周期、项目成员等因素，预估工作时间。</span></span>
+<span class="line"><span>2. 预估工作量：根据项目需求、项目周期、项目成员等因素，预估工作量。</span></span></code></pre></div><h2 id="第-51-周" tabindex="-1">第 51 周 <a class="header-anchor" href="#第-51-周" aria-label="Permalink to &quot;第 51 周&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>周二（计划）</span></span></code></pre></div>`,6)]])}var s=r(a,[[`render`,o]]);export{i as __pageData,s as default};
