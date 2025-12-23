2f0be69dc560b432243414570f984805206d9e85
小福 <995333824@qq.com>
2025-12-24 02:32:59 +0800

    Updates

---
+++ assets/doc_MyselfInfo_interview_review.md.BT4vHlpC.js
@@ -0,0 +1,22 @@
+import{At as e,ct as t,s as n,ut as r}from"./chunks/framework.DGAZ-G8q.js";const i=JSON.parse(`{"title":"中铁十二局数字土木工程院，面试提到的问题","description":"","frontmatter":{},"headers":[],"relativePath":"doc/MyselfInfo/interview/review.md","filePath":"doc/MyselfInfo/interview/review.md"}`);var a={name:`doc/MyselfInfo/interview/review.md`};function o(n,i,a,o,s,c){return e(),t(`div`,null,[...i[0]||=[r(`<h1 id="中铁十二局数字土木工程院-面试提到的问题" tabindex="-1">中铁十二局数字土木工程院，面试提到的问题 <a class="header-anchor" href="#中铁十二局数字土木工程院-面试提到的问题" aria-label="Permalink to &quot;中铁十二局数字土木工程院，面试提到的问题&quot;">​</a></h1><p>业务方向：智慧工地 面试中没有回答上的问题</p><p>一、 Vue（react 中 mvm 思想理解?）</p><ol><li>海量的数据列表，渲染中的部分列表数据性能优化如何做？</li><li>谈一下 vue 中 dom 的渲染机理？</li><li>组件化封装，echart 封装都遇到了哪些问题？</li><li>讲解 promise 实现过程，promise 遇到的项目难点</li><li>大文件上传如何处理</li><li>Provide 和 inject 讲解一下，实现机理 通过回答这个问题，我发现自己的回答面试的思路模板有问题嗯，正常应该按何一老师那样应该这样回答， 1.说明 provide 和 inject 的呃功能 第 21 点是说明相似功能的 API 有哪些？ 第 3 点是说这 API 之间的区别，然后第 4 点是说在哪些应用场景下，使用 provide 和 inject</li></ol><p>二、git 和架构设计 git 使用 monorepo 架构理解-&gt;如何实现项目中功能的复用</p><p>三、三维</p><ol><li>cesium 中如何处理两份不同的 dem 加载？</li><li>坐标系转换 坐标转换流程，用到的工具</li><li>数据按照顺序加载 4.地图影像服务用过哪些？和商用的地图服务有做过横向对比吗</li></ol><p>四、webgl</p><ol><li>渲染管线讲解数据 docker 容器化服务 4</li></ol><p>五、简历（借助 AI 填补经验空白）</p><ol><li>担任组长.被问及不会回答-&gt;</li><li>架构师-&gt;在项目研发中负责的工作列举</li><li>架构师核心能力 <ul><li>技术架构（项目架构设计，项目技术选型，项目技术实现）</li><li>业务架构 （业务架构设计，业务功能实现）</li><li>技术文档（项目技术文档，项目接口文档，项目需求文档）</li></ul></li></ol><p>这次面试中得到的启示（刨根问底-页面实现简单 demo）</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>性能优化：</span></span>
+<span class="line"><span>在平时的学习过程中，首先解决当前项目的性能问题</span></span>
+<span class="line"><span>1.vue 中虚拟dom 的渲染</span></span>
+<span class="line"><span>2.拓展（性能优化）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>前端</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>- 渲染层面</span></span>
+<span class="line"><span>  - react 中虚拟dom 的渲染</span></span>
+<span class="line"><span>  - js 如何实现渲染性能优化</span></span>
+<span class="line"><span>      - 三维层面</span></span>
+<span class="line"><span>            - cesium</span></span>
+<span class="line"><span>            - three</span></span>
+<span class="line"><span>            - webgl</span></span>
+<span class="line"><span>            - 拓展 webgl 性能优化</span></span>
+<span class="line"><span>- 数据查询层</span></span>
+<span class="line"><span>- 缓存层面</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>后端</span></span>
+<span class="line"><span>- 数据库层面</span></span>
+<span class="line"><span>  - 数据库索引优化</span></span>
+<span class="line"><span>  - 数据库分表分库</span></span></code></pre></div>`,13)]])}var s=n(a,[[`render`,o]]);export{i as __pageData,s as default};
